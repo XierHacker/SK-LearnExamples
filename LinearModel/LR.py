135b211bdb8ba3d6e6ebeb0e85705c2395b2b3fb
XierHacker <812760979@163.com>
2017-04-27 18:50:47 +0800

    initial commit

--- LinearModel/LR.py
+++ LinearModel/LR.py
@@ -1,6 +1,9 @@
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import log_loss
 
 #load data
 iris=load_iris()
@@ -10,7 +13,31 @@ labels=iris.target
 #print(dataSet.shape)
 #print(labels.shape)
 
-#
+#split dataSet
+train_dataSet,test_dataSet,train_labels,test_labels=train_test_split(dataSet,labels,test_size=50)
+#train model
 LR=LogisticRegression(n_jobs=-1)
+LR.fit(X=train_dataSet,y=train_labels)
+
+
+#predict
+result=LR.predict(X=test_dataSet)
+prob=LR.predict_proba(X=test_dataSet)
+
+print("groud truth:\n",test_labels)
+print("result:\n",result)
+print("prob:\n",prob)
+
+#accuracy
+acc=accuracy_score(y_true=test_labels,y_pred=result)
+print("Accuracy:",acc)
+
+#log loss
+LogLoss=log_loss(y_true=test_labels,y_pred=prob)
+print("Log Loss:",LogLoss)
+
+
+
+
 
 
--- Metrics/classification.py
+++ Metrics/classification.py
@@ -1,9 +1,12 @@
 import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
+
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import log_loss
+
 
 #load data
 iris=load_iris()
@@ -23,6 +26,8 @@ decisionTree=DecisionTreeClassifier()
 decisionTree.fit(X=train_dataSet,y=train_labels)
 
 predict=decisionTree.predict(X=test_dataSet)
+prob=decisionTree.predict_proba(X=test_dataSet)
+print(prob)
 #print(predict)
 #print("\n\n\n")
 #print(test_labels)
@@ -34,3 +39,7 @@ print(accuracy)
 #confusion matrix
 ConfuMat=confusion_matrix(y_true=test_labels,y_pred=predict)
 print(ConfuMat)
+
+#log loss
+LogLoss=log_loss(y_true=test_labels,y_pred=prob)
+print(LogLoss)
