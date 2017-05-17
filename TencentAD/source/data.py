4a17461bb125492f9686cf6426655940e5597550
XierHacker <xierhacker@163.com>
2017-05-17 11:52:20 +0800

    initial commit

--- TencentAD/source/data.py
+++ TencentAD/source/data.py
@@ -4,19 +4,28 @@ import numpy as np
 def loadFrame():
     train_frame = pd.read_csv("../data/pre/train.csv")
     train_labels_frame=train_frame.pop("label")
-    test_frame = pd.read_csv("../data/pre/test.csv")
+    test_frame = pd.read_csv("../data/pre/test.csv",index_col="instanceID")
+    test_frame.pop("label")
     return train_frame,train_labels_frame,test_frame
 
 
+def loadUserFrame():
+    user_frame = pd.read_csv("../data/pre/user.csv")
+    return user_frame
+
+def loadAdFrame():
+    ad_frame = pd.read_csv("../data/pre/ad.csv")
+    return ad_frame
+
+def loadPositionFrame():
+    position_frame = pd.read_csv("../data/pre/position.csv")
+    return position_frame
+
 
 def loadExtraFrame():
-    ad_frame = pd.read_csv("../data/pre/ad.csv")
     app_categories_frame = pd.read_csv("../data/pre/app_categories.csv")
-    position_frame = pd.read_csv("../data/pre/position.csv")
-    user_frame = pd.read_csv("../data/pre/user.csv")
     user_app_actions_frame = pd.read_csv("../data/pre/user_app_actions.csv")
     user_installedapps_frame = pd.read_csv("../data/pre/user_installedapps.csv")
-    return ad_frame, app_categories_frame, position_frame, \
-           user_frame, user_app_actions_frame, user_installedapps_frame
+    return app_categories_frame,user_app_actions_frame, user_installedapps_frame
 
 
