af53ed6a5b4b8cab4c4dec49cf7b0db3982bc38c
XierHacker <xierhacker@163.com>
2017-05-18 09:47:32 +0800

    initial commit

--- TencentAD/source/data.py
+++ TencentAD/source/data.py
@@ -4,7 +4,7 @@ import numpy as np
 def loadFrame():
     train_frame = pd.read_csv("../data/pre/train.csv")
     train_labels_frame=train_frame.pop("label")
-    test_frame = pd.read_csv("../data/pre/test.csv",index_col="instanceID")
+    test_frame = pd.read_csv("../data/pre/test.csv")
     test_frame.pop("label")
     return train_frame,train_labels_frame,test_frame
 
@@ -21,11 +21,17 @@ def loadPositionFrame():
     position_frame = pd.read_csv("../data/pre/position.csv")
     return position_frame
 
-
-def loadExtraFrame():
+def loadAppCategoriesFrame():
     app_categories_frame = pd.read_csv("../data/pre/app_categories.csv")
+    return app_categories_frame
+
+
+def loadUserAppActionsFrame():
     user_app_actions_frame = pd.read_csv("../data/pre/user_app_actions.csv")
+    return user_app_actions_frame
+
+def loadUserInstalledAppsFrame():
     user_installedapps_frame = pd.read_csv("../data/pre/user_installedapps.csv")
-    return app_categories_frame,user_app_actions_frame, user_installedapps_frame
+    return user_installedapps_frame
 
 
